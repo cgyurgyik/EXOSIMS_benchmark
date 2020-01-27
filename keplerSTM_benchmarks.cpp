9f75b3d94807c0ec7cc0dd03a2945cf092bab752
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-26 20:34:38 -0500

    Cleanup.

--- keplerSTM_benchmarks.cpp
+++ keplerSTM_benchmarks.cpp
@@ -248,21 +248,3 @@ BENCHMARK(Old_KeplerSTM);
 BENCHMARK(New_KeplerSTM);
 
 BENCHMARK_MAIN();
-
-/*
-
-Run on (4 X 1600 MHz CPU s)
-CPU Caches:
-L1 Data 32 KiB (x2)
-L1 Instruction 32 KiB (x2)
-L2 Unified 256 KiB (x2)
-L3 Unified 3072 KiB (x1)
-Load Average: 4.15, 3.13, 3.06
-***WARNING*** Library was built as DEBUG. Timings may be affected.
---------------------------------------------------------
-Benchmark              Time             CPU   Iterations
---------------------------------------------------------
-Old_KeplerSTM       9422 ns         8759 ns        74960
-New_KeplerSTM       7612 ns         7298 ns        84318
-
-*/
